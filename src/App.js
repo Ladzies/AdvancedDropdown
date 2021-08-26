1cebeb1ed11ff01a224770a585f98f994c9f3bb8
Alex Lazarian <34932547+Ladzies@users.noreply.github.com>
2021-08-26 11:09:03 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import {ReactComponent as DropdownIcon} from './icons/dropdown.svg'
+import {ReactComponent as RightChevron} from './icons/right-chevron.svg'
+import React, {useState} from 'react'
+
 function App() {
 	return (
 		<Navbar>
-			<NavItem icon='B'>hello</NavItem>
+			<NavItem icon={<DropdownIcon />}>
+				<DropDownMenu />
+			</NavItem>
 		</Navbar>
 	)
 }
@@ -14,13 +20,41 @@ function Navbar({children}) {
 	)
 }
 
-function NavItem({icon}) {
+function NavItem({children, icon}) {
+	const [open, setOpen] = useState(true)
 	return (
 		<li className='nav-item'>
-			<a href='#' className='icon-button'>
+			<a
+				href='#'
+				className='icon-button'
+				onClick={() => setOpen(!open)}
+			>
 				{icon}
 			</a>
+			{open && children}
 		</li>
+	)
+}
+
+function DropDownMenu() {
+	function DropDownItem({children, leftIcon, rightIcon}) {
+		return (
+			<a href='#' className='menu-item'>
+				<span className='icon-button'>{leftIcon}</span>
+				{children}
+				<span className='icon-button'>{rightIcon}</span>
+			</a>
+		)
+	}
+	return (
+		<div className='dropdown'>
+			<DropDownItem rightIcon={<RightChevron />}>
+				My profile
+			</DropDownItem>
+			<DropDownItem
+				leftIcon={<DropdownIcon />}
+			></DropDownItem>
+		</div>
 	)
 }
 
