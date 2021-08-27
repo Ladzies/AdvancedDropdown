6516a93348da2a564818c1326994c07ecc19c8e8
Alex Lazarian <34932547+Ladzies@users.noreply.github.com>
2021-08-26 18:34:52 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import {ReactComponent as DropdownIcon} from './icons/dropdown.svg'
 import {ReactComponent as RightChevron} from './icons/right-chevron.svg'
+import {ReactComponent as HomeIcon} from './icons/home.svg'
+import {ReactComponent as DiscordIcon} from './icons/discord.svg'
+import {ReactComponent as BillingIcon} from './icons/bill.svg'
+import {ReactComponent as AddIcon} from './icons/add.svg'
+import {ReactComponent as LogoutIcon} from './icons/logout.svg'
+import {ReactComponent as BackArrow} from './icons/left-arrow.svg'
+
 import React, {useState} from 'react'
+import {CSSTransition} from 'react-transition-group'
+// import ReactCSSTransitionGroup from 'react-transition-group'
 
 function App() {
 	return (
@@ -26,7 +35,7 @@ function NavItem({children, icon}) {
 		<li className='nav-item'>
 			<a
 				href='#'
-				className='icon-button'
+				className='icon-button --circle-button'
 				onClick={() => setOpen(!open)}
 			>
 				{icon}
@@ -37,23 +46,91 @@ function NavItem({children, icon}) {
 }
 
 function DropDownMenu() {
-	function DropDownItem({children, leftIcon, rightIcon}) {
+	const [activeMenu, setActiveMenu] = useState('main')
+	const [menuHeight, setMenuHeight] = useState(null)
+
+	// let firstComuted = 0
+
+	function getHeight(el) {
+		setMenuHeight(el.parentNode.offsetHeight)
+	}
+
+	function DropDownItem({
+		children,
+		leftIcon,
+		rightIcon,
+		...props
+	}) {
 		return (
-			<a href='#' className='menu-item'>
-				<span className='icon-button'>{leftIcon}</span>
-				{children}
-				<span className='icon-button'>{rightIcon}</span>
+			<a href='#' className='menu-item' {...props}>
+				<div className='menu-item-group'>
+					<span className='icon-button --menu-button-left'>
+						{leftIcon}
+					</span>
+					{children}
+				</div>
+				<span className='icon-button --menu-button-right'>
+					{rightIcon}
+				</span>
 			</a>
 		)
 	}
+
 	return (
-		<div className='dropdown'>
-			<DropDownItem rightIcon={<RightChevron />}>
-				My profile
-			</DropDownItem>
-			<DropDownItem
-				leftIcon={<DropdownIcon />}
-			></DropDownItem>
+		<div className='dropdown' style={{height: menuHeight}}>
+			<CSSTransition
+				in={activeMenu === 'main'}
+				unmountOnExit
+				timeout={500}
+				classNames='menu-primary'
+				onEnter={getHeight}
+			>
+				<div className='menu'>
+					<DropDownItem
+						leftIcon={<img src='./images/profile.jpeg' />}
+						rightIcon={<RightChevron />}
+						onClick={() => setActiveMenu('profile')}
+					>
+						Edit Profile
+					</DropDownItem>
+					<div className='menu-separator'></div>
+					<DropDownItem leftIcon={<HomeIcon />}>
+						Dashboard
+					</DropDownItem>
+					<DropDownItem leftIcon={<DiscordIcon />}>
+						Open Discord
+					</DropDownItem>
+					<DropDownItem leftIcon={<BillingIcon />}>
+						Billing
+					</DropDownItem>
+					<DropDownItem leftIcon={<AddIcon />}>
+						New scrim
+					</DropDownItem>
+					<div className='menu-separator'></div>
+					<DropDownItem leftIcon={<LogoutIcon />}>
+						Log out
+					</DropDownItem>
+				</div>
+			</CSSTransition>
+
+			<CSSTransition
+				in={activeMenu === 'profile'}
+				unmountOnExit
+				timeout={500}
+				classNames='menu-secondary'
+				onEnter={setMenuHeight(100)} //firstComuted
+			>
+				<div className='menu'>
+					<DropDownItem
+						leftIcon={<BackArrow />}
+						onClick={() => setActiveMenu('main')}
+					>
+						Edit Profile
+					</DropDownItem>
+					<input />
+					<textarea />
+				</div>
+			</CSSTransition>
 		</div>
 	)
 }
